683abe93f48f0dfc0445c9fe9e8f8dc4de23850b
BraMenC28 <pipe28men@gmail.com>
2026-08-12 00:05:20 -0500

    Mejora de codigo en general(condiciones de numeros dependiendo del operador)

--- CalculadoraPrin.py
+++ CalculadoraPrin.py
@@ -8,93 +8,102 @@ miclasEspe=CalculadoraEspecial(0,0,0,0)
 numActual=0
 ciclos=0
 continuar ="s"
+reciclado=None
+opcion=None
 while (continuar=="s"):
-    print(" 1.Suma \n 2. resta \n 3. multiplicacion \n 4. divicion \n 5. seno \n 6. coseno \n 7. tangente \n 8. factorial \n 9. maximo comun divisor \n 10. minimo comun multiplo \n 11. IVA \n 12. potencia \n 13. raiz \n 14. fibonacci")
+    print(" 1. Suma \n 2. Resta \n 3. Multiplicacion \n 4. Division \n 5. Seno \n 6. Coseno \n 7. Tangente \n 8. Factorial \n 9. Maximo comun divisor \n 10. Minimo comun multiplo \n 11. IVA \n 12. Potencia \n 13. Raiz \n 14. Fibonacci \n 15. salir")
+    print("Para las operaciones de suma, resta, multiplicacion y division puede usar el resultado de la operacion anterior como primer numero \n")
     opcion=int(input("Escoja una de las opciones :"))
-   
-    miCalcBasic.num_1=miCalcBasic.resultado
-
-    if opcion==1 and ciclos==0:
+    if opcion==1 and ciclos==0 : #compara la opcion a elegir, si ya paso por el primer ciclo para almacenar el ultimo resultado
         miCalcBasic.num_1=int(input("ingrese el numero 1 :"))
         miCalcBasic.num_2=int(input("ingrese el numero 2 :"))
         miCalcBasic.suma()
         print("la suma es: ", miCalcBasic.resultado)
-    elif opcion==1 and ciclos!=0:
-        miCalcBasic.num_1=numActual
-        print ("su numero uno ahora es", miCalcBasic.num_1)
-        miCalcBasic.num_2=int(input("ingrese el numero 2 :"))
-        miCalcBasic.suma()
-        print("la suma es: ", miCalcBasic.resultado)
-       
-       
-       
+    elif opcion==1 and ciclos!=0 : #comara la opcion a elegir, si ya paso por el primer ciclo para almacenar el ultimo resultado
+        reciclado=input("Desea usar el ultimo resultado (s/n): ")
+        if reciclado=="s":
+            miCalcBasic.num_1=numActual
+            print ("su numero uno ahora es", miCalcBasic.num_1)
+            miCalcBasic.num_2=int(input("ingrese el numero 2 :"))
+            miCalcBasic.suma()
+            print("la suma es: ", miCalcBasic.resultado)
+        else:
+            miCalcBasic.num_1=int(input("ingrese el numero 1 :"))
+            miCalcBasic.num_2=int(input("ingrese el numero 2 :"))
+            miCalcBasic.suma()
+            print("la suma es: ", miCalcBasic.resultado)
     elif opcion==2 and ciclos==0:
         n1=int(input("ingrese el numero 1 :"))
         n2=int(input("ingrese el numero 2 :"))
         print("la resta es: ", miCalcBasic.resta(n1,n2))
-       
     elif opcion==2 and ciclos!=0:
-        n1=numActual
-        print ("su numero uno ahora es", n1)
-        n2=int(input("ingrese el numero 2 :"))
-        print("la resta es: ", miCalcBasic.resta(n1,n2))
-
-
-
+        reciclado=input("Desea usar el ultimo resultado (s/n): ")
+        if reciclado=="s":
+            n1=numActual
+            print ("su numero uno ahora es", n1)
+            n2=int(input("ingrese el numero 2 :"))
+            print("la resta es: ", miCalcBasic.resta(n1,n2))
+        else:
+            n1=int(input("ingrese el numero 1 :"))
+            n2=int(input("ingrese el numero 2 :"))
+            print("la resta es: ", miCalcBasic.resta(n1,n2))
     elif opcion==3 and ciclos==0:
         miCalcBasic.num_1=int(input("ingrese el numero 1 :"))
         miCalcBasic.num_2=int(input("ingrese el numero 2 :"))
         miCalcBasic.multiplicacion()
         print("la multiplicacion es: ", miCalcBasic.resultado)
-       
     elif opcion==3 and ciclos!=0:
-        miCalcBasic.num_1=numActual
-        print ("su numero uno ahora es", miCalcBasic.num_1)
-        miCalcBasic.num_2=int(input("ingrese el numero 2 :"))
-        miCalcBasic.multiplicacion()
-        print("la multiplicacion es: ", miCalcBasic.resultado)
-       
-       
-
+        reciclado=input("Desea usar el ultimo resultado (s/n): ")
+        if reciclado=="s":
+            miCalcBasic.num_1=numActual
+            print ("su numero uno ahora es", miCalcBasic.num_1)
+            miCalcBasic.num_2=int(input("ingrese el numero 2 :"))
+            miCalcBasic.multiplicacion()
+            print("la multiplicacion es: ", miCalcBasic.resultado)
+        else:
+            miCalcBasic.num_1=int(input("ingrese el numero 1 :"))
+            miCalcBasic.num_2=int(input("ingrese el numero 2 :"))
+            miCalcBasic.multiplicacion()
+            print("la multiplicacion es: ", miCalcBasic.resultado)
     elif opcion==4 and ciclos==0:
-        miCalcBasic.num_1=int(input("ingrese el numero 1 :"))
-        miCalcBasic.num_2=int(input("ingrese el numero 2 :"))
+        miCalcBasic.num_1=int(input("ingrese el numero 1 (numerador) :"))
+        miCalcBasic.num_2=int(input("ingrese el numero 2 (denominador):"))
         if miCalcBasic.num_2==0:
             print("no se puede dividir entre 0")
         else:
             miCalcBasic.divicion()
             print("la divicion es: ", miCalcBasic.resultado)
     elif opcion==4 and ciclos!=0:
-        miCalcBasic.num_1=numActual
-        print ("su numero uno ahora es", miCalcBasic.num_1)
-        miCalcBasic.num_2=int(input("ingrese el numero 2 :"))
-        if miCalcBasic.num_2==0:
-            print("no se puede dividir entre 0")
+        reciclado=input("Desea usar el ultimo resultado (s/n): ")
+        if reciclado=="s":
+            miCalcBasic.num_1=numActual
+            print ("su numero uno ahora es", miCalcBasic.num_1)
+            miCalcBasic.num_2=int(input("ingrese el numero 2 :"))
+            if miCalcBasic.num_2==0:
+                print("no se puede dividir entre 0")
+            else:
+                miCalcBasic.divicion()
+                print("la divicion es: ", miCalcBasic.resultado)
         else:
-            miCalcBasic.divicion()
-            print("la divicion es: ", miCalcBasic.resultado)
-
-
-
+            miCalcBasic.num_1=int(input("ingrese el numero 1 (numerador) :"))
+            miCalcBasic.num_2=int(input("ingrese el numero 2 (denominador):"))
+            if miCalcBasic.num_2==0:
+                print("no se puede dividir entre 0")
+            else:
+                miCalcBasic.divicion()
+                print("la divicion es: ", miCalcBasic.resultado)
     elif opcion==5:
         angulo=float(input("ingrese el ángulo en radianes: "))
         miCalcTrigo.seno(angulo)
-
-    
     elif opcion==6:
         angulo=float(input("ingrese el angulo en radianes: "))
         miCalcTrigo.cos(angulo)
-
-
     elif opcion==7:
         angulo=float(input("ingrese el ángulo en radianes: "))
         miCalcTrigo.tan(angulo)
-
     elif opcion==8:
        miclasEspe.num_1=int(input("ingrese el numero al que desea calcular el factorial: "))
        miclasEspe.factorial()
-       print("El factorial es: ", miclasEspe.resultado)
-
     elif opcion==9:
       miclasEspe.num_1=int(input("ingrese el primer numero: "))
       miclasEspe.num_2=int(input("ingrese el segundo numero: "))
@@ -109,7 +118,7 @@ while (continuar=="s"):
       print("El precio total con IVA es: ", miclasEspe.resultado) 
     elif opcion==12:
         miclasEspe.num_1=float(input("ingrese la base de la potencia: "))
-        miclasEspe.num_2=int(input("ingrese el exponente de la potencia: "))
+        miclasEspe.num_2=int(input("ingrese el exponente de la potencia (entero): "))
         miclasEspe.potencia()
     elif opcion==13:
         miclasEspe.num_1=float(input("ingrese el indice: "))
@@ -118,7 +127,8 @@ while (continuar=="s"):
     elif opcion==14:
         miclasEspe.num_1=int(input("ingrese la posicion del numero de fibonacci que desea conocer (La cuenta inicia desde el 0): "))
         print("El numero de la posicion es", miclasEspe.fibonacci())
-
+    elif opcion==15:
+        continuar="n"
     else:
      print("error")
     continuar=input("desea continuar? s/n :" )
--- Especi.py
+++ Especi.py
@@ -5,14 +5,12 @@ class CalculadoraEspecial:
         self.num_2=num_2
         self.resultado=resultado
         self.Precio=precio
-
     def potencia(self):
        if self.num_2<0 and self.num_1==0:
           print("El signo del exponente y la base no permiten una divicion por cero")
        else:
         self.resultado=self.num_1**self.num_2
         print ("El resultado de la potencia es:", self.resultado)
-
     def raiz(self):#num_1=indice, num_2=argumento
         if self.num_1==0:
             print("No se puede sacar raiz a un numeor evaluado con un indice igua a cero")
@@ -24,7 +22,6 @@ class CalculadoraEspecial:
         else:
             resultado=self.num_2**(1/self.num_1)
             print("El resultado es", resultado)
-
     def fibonacci(self):
         i=0
         fibo=[0,1]
@@ -32,11 +29,12 @@ class CalculadoraEspecial:
             aumento=fibo[-1]+fibo[-2]
             fibo.append(aumento)
         return fibo[self.num_1-1]
-
     def factorial(self):
         if self.num_1<0:
             print("No se puede calcular el factorial de un numero negativo")
-            self.resultado = None
+        elif self.num_1==0:
+           self.resultado=1
+           print("El factorial es: ", self.resultado)
         else:
            menor=None
            while menor!=1:
@@ -47,13 +45,22 @@ class CalculadoraEspecial:
               menor=menor-1
              self.resultado=self.num_1*menor
              self.num_1=self.resultado
-        return self.resultado
-    
+           print("El factorial es: ", self.resultado)
     def MaxComDiv(self):
         if  self.num_1<0:
             self.num_1=-1*self.num_1
         if self.num_2<0:
             self.num_2=-1*self.num_2
+        if abs(self.num_1)==abs(self.num_2) and self.num_2!=0 and self.num_1!=0:
+             self.resultado=abs(self.num_2)
+             return self.resultado
+        if self.num_2==0 and self.num_1==0:
+           return None
+        if  self.num_2==0 or self.num_1==0:
+           if self.num_2==0:
+              return self.num_1
+           else:
+              return self.num_2
         if self.num_1>self.num_2 and self.num_2!=0:
             mayor=self.num_1
             menor=self.num_2
@@ -70,8 +77,7 @@ class CalculadoraEspecial:
             return self.resultado
         else:
             print("No existe un maximo comun divisor")
-            return
-        
+            return  
     def MinComMul(self):
         if self.num_1*self.num_2 <0:
             self.resultado= ((self.num_1*self.num_2 * -1)/self.MaxComDiv())
@@ -79,8 +85,6 @@ class CalculadoraEspecial:
         else:
             self.resultado= ((self.num_1*self.num_2)/self.MaxComDiv())
         return self.resultado
-
     def IVA(self):
-        self.resultado = (self.Precio * 0.19)+self.Precio
-
-
+        iva=float(input("ingrese el valor del IVA: "))
+        self.resultado = (self.Precio * iva)+self.Precio
